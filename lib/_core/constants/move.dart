66647e76bcc6938cb280b8122f8f5c0901c13300
sji8562 <whaqlzkem@gmail.com>
2023-10-20 16:35:35 +0900

    DetailForm

--- lib/_core/constants/move.dart
+++ lib/_core/constants/move.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toyproject/ui/pages/costomer/detail_page/comtomer_detail_page.dart';
 
 
 class Move {
@@ -11,7 +12,7 @@ class Move {
 
 Map<String, Widget Function(BuildContext)> getRouters() {
   return {
-    // Move.loginPage: (context) => const LoginPage(),
+    Move.loginPage: (context) => const CostomerDetailPage(),
     // Move.joinPage: (context) => const JoinPage(),
     // Move.postListPage: (context) => PostListPage(),
     // Move.postWritePage: (context) => const PostWritePage(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '_core/constants/move.dart';
+import '_core/constants/theme.dart';
+
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,7 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      navigatorKey: navigatorKey, // context가 없는 곳에서 context를 사용할 수 있는 방법 (몰라도 됨)
+      debugShowCheckedModeBanner: false,
+      initialRoute: Move.loginPage,
+      routes: getRouters(),
+      theme: theme(),
     );
   }
 }
